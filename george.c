b391c2990e725c82737826fea6303c6103028f63
George Silvis, III <george.iii.silvis@gmail.com>
2014-01-14 08:40:31 +0000

    Dynamically allocate stack threads

--- george.c
+++ george.c
@@ -7,6 +7,7 @@
 #include "isr.h"
 #include "pic.h"
 #include "page.h"
+#include "alloc.h"
 
 void forever_yielding (void *a) {
   char c = (char)(unsigned int) a;
@@ -24,8 +25,6 @@ void forever_unyielding (void *a) {
   }
 }
 
-int stacks[5][1024];
-
 void initialize_idt (void) {
   for (int i = 0; i < 256; i++) {
     register_isr(i, isr_other);
@@ -38,6 +37,17 @@ void initialize_idt (void) {
   register_isr(0x21, kbd_isr);
 }
 
+void create_test_threads (void) {
+  int *(stacks[4]);
+  for (int i = 0; i < 4; i++) {
+    stacks[i] = allocate_page();
+  }
+  thread_create(&stacks[0][1024], forever_yielding, (void *)'u');
+  thread_create(&stacks[1][1024], forever_yielding, (void *)'v');
+  thread_create(&stacks[2][1024], forever_unyielding, (void *)'A');
+  thread_create(&stacks[3][1024], forever_unyielding, (void *)'B');
+}
+
 void kernel_main (int magic, unsigned int *mboot_struct) {
   clear_screen();
   if (magic != 0x2BADB002) {
@@ -45,10 +55,6 @@ void kernel_main (int magic, unsigned int *mboot_struct) {
     return;
   }
 
-  thread_create(&stacks[0][1024], forever_yielding, (void *)'u');
-  thread_create(&stacks[1][1024], forever_yielding, (void *)'v');
-  thread_create(&stacks[2][1024], forever_unyielding, (void *)'A');
-  thread_create(&stacks[3][1024], forever_unyielding, (void *)'B');
 
   puts("Welcome to GeorgeOS, Multiboot Edition!\r\n");
   puts("Total Available Ram: ");
@@ -56,7 +62,7 @@ void kernel_main (int magic, unsigned int *mboot_struct) {
   puts(" MB\r\n");
 
   puts("Initializing memory allocator\r\n");
-  initialize_allocator();
+  initialize_allocator(mboot_struct[2]);
   puts("Initializing IDT\r\n");
   initialize_idt();
   puts("Inserting IDT\r\n");
@@ -67,6 +73,8 @@ void kernel_main (int magic, unsigned int *mboot_struct) {
   remap_pic();
   puts("Enabling paging\r\n");
   enable_paging();
+  puts("Creating test threads\r\n");
+  create_test_threads();
   puts("Enabling hardware interrupts\r\n");
   sti();
   puts("Initializing thread subsystem\r\n");
