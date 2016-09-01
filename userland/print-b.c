93467c12f9f1907ec57521d8c0ef494bdd923240
George Silvis, III <george.iii.silvis@gmail.com>
2016-08-31 22:21:43 -0400

    Fix linker script
    Now pointers should work correctly.

--- userland/print-b.c
+++ userland/print-b.c
@@ -1,7 +1,7 @@
 #include "userland.h"
 
 void main (void) {
-  char str[] = "I'M REALLY ANNOYING";
+  char *str = "I'M REALLY ANNOYING";
   for (unsigned int i = 0; i < 19; i++) {
     putch(str[i]);
     __asm__ volatile("mov $50000000,%%ecx; \
