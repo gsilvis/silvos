5fb5a81393a465c62293ad743eb00fb63849f796
George Silvis, III <george.iii.silvis@gmail.com>
2017-01-06 21:27:42 -0500

    Simplify module manipulation in bootloader
    Before, it made fairly week assumptions, but used dubious untested code to
    make it work.  Now it has a very simple, strong assumption: All of the
    multiboot things are below the address that the kernel will be loaded at.
    
    The advantage is that now modules don't need to be moved around, and their
    names will survive.
    
    This can be revisited if the kernel gets larger, but it's definitely
    sufficient for now.

--- bootloader/start64.c
+++ bootloader/start64.c
@@ -2,17 +2,11 @@
 
 #define MAP_OFFSET 0xFFFFFFFF80000000
 
-void memmove (uint64_t dest, uint64_t src, uint64_t n) {
+void memcpy (uint64_t dest, uint64_t src, uint64_t n) {
   uint8_t *d = (uint8_t *)dest;
   uint8_t *s = (uint8_t *)src;
-  if (dest > src) {
-    for (uint64_t i = 0; i < n; i++) {
-      d[n-i-1] = s[n-i-1];
-    }
-  } else if (dest < src) {
-    for (uint64_t i = 0; i < n; i++) {
-      d[i] = s[i];
-    }
+  for (uint64_t i = 0; i < n; i++) {
+    d[i] = s[i];
   }
 }
 
@@ -37,12 +31,9 @@ typedef struct {
 } multiboot_module;
 
 uint64_t relocate_kernel (uint32_t mboot_struct_addr) {
-  /* Procedure:  Find out where the kernel will end up.  For each module that
-   * overlaps that, copy it past the end.  Then move the kernel into place.
-   * Finally, copy the displaced modules back to where the kernel used to be.
-   *
-   * Assumptions:  That the modules are all AFTER the multiboot header and the
-   * module list.  That no one cares about module names.
+  /*
+   * Assumption:  All multiboot modules and tables are below where the kernel
+   * asks to be loaded.
    */
   uint32_t *mboot_struct = (uint32_t *)(uint64_t)mboot_struct_addr;
   if (mboot_struct[5] == 0) {
@@ -70,46 +61,19 @@ uint64_t relocate_kernel (uint32_t mboot_struct_addr) {
     fail(); /* We do not support parsing ELF */
   }
 
-  uint32_t old_kernel_loc = mod_list[0].start;
   uint32_t header_addr = kernel[i+3];
   uint32_t load_addr = kernel[i+4];
   uint32_t load_end_addr = kernel[i+5];
   uint32_t bss_end_addr = kernel[i+6];
   uint32_t entry_addr = kernel[i+7];
 
-  /* First, copy all the modules the kernel will interfere with to after the
-   * kernel. */
-  uint32_t copy_to_addr = bss_end_addr;
-  for (uint32_t j = 1; j < mboot_struct[5]; j++) {
-    if (mod_list[j].start < bss_end_addr && mod_list[j].end > load_addr) {
-      uint32_t length = mod_list[j].end - mod_list[j].start;
-      memmove(copy_to_addr, mod_list[j].start, length);
-      mod_list[j].start = copy_to_addr;
-      mod_list[j].end = copy_to_addr + length;
-    }
-  }
-
   /* Move the kernel */
-  memmove(load_addr,
-          ((uint64_t)&kernel[i]) + load_addr - header_addr,
-          load_end_addr - load_addr);
+  memcpy(load_addr,
+         ((uint64_t)&kernel[i]) + load_addr - header_addr,
+         load_end_addr - load_addr);
   memset(load_end_addr, 0, bss_end_addr - load_end_addr);
 
-  /* Move the modules back */
-  copy_to_addr = old_kernel_loc;
-  for (uint32_t j = 1; j < mboot_struct[5]; j++) {
-    if (mod_list[j].start >= bss_end_addr) {
-      uint32_t length = mod_list[j].end - mod_list[j].start;
-      memmove(copy_to_addr, mod_list[j].start, length);
-      mod_list[j].start = copy_to_addr;
-      mod_list[j].end = copy_to_addr + length;
-    }
-  }
-
   /* Fix up the list of modules */
-  for (uint32_t j = 0; j < mboot_struct[5]; j++) {
-    mod_list[j].string = 0; /* These got clobbered anyways */
-  }
   mboot_struct[5]--;
   mboot_struct[6] += sizeof(multiboot_module);
 
