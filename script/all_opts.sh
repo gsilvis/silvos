4b53a672219ed67ff84b9f920563579c76362438
Allan Wirth <allanlw@gmail.com>
2017-07-30 01:14:23 +0000

    reduce number of optimization settings tried

--- script/all_opts.sh
+++ script/all_opts.sh
@@ -2,11 +2,9 @@
 
 set -eu
 
-for oflag in -O0 -O2 -O3 -Os; do
-  for ltoflag in -flto ''; do
-    for uoflag in -O0 -O2; do
-      make clean
-      USER_OPT="$uoflag" KERNEL_OPT="$oflag -g $ltoflag " make -j $(nproc) test
-     done
-  done
+for oflag in '-O0' '-O2' '-O3' '-O3 -flto'; do
+  for uoflag in -O0 -O2; do
+    make clean
+    USER_OPT="$uoflag" KERNEL_OPT="$oflag -g" make -j $(nproc) test
+   done
 done
