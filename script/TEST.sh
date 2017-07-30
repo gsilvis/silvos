bf78b358fddd5804a32a5b03d208ccfb8105e543
Colin Stanfill <colin.stanfill@gmail.com>
2017-07-29 23:13:27 -0400

    Some more convenience scripts
    symbolize.sh is a pipeline using addr2line that will make the stack
    traces look nicer.

---
+++ script/TEST.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+MODULES="$(join_by , george.multiboot "$@")"
+
+qemu-system-x86_64 -kernel bootloader.multiboot -initrd "$MODULES" -drive file=temp_drive,index=0,media=disk,format=raw -s -serial stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04
