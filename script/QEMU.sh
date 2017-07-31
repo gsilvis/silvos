2d0deece96195b4d4a4526b6367cdbde0b7028bb
Allan Wirth <allanlw@gmail.com>
2017-07-31 03:24:46 +0000

    Updated Makefile to use QEMU.sh too

--- script/QEMU.sh
+++ script/QEMU.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTIONS=$(getopt -n "$0" -o hS: --long "help,wait:" -- "$@")
+OPTIONS=$(getopt -n "$0" -o hSs:d:c: --long "help,wait,serial:,nographic,drive:,coverage:" -- "$@")
 
 if [ $? -ne 0 ]; then
   echo "Failed to parse args"
@@ -12,30 +12,49 @@ eval set -- "$OPTIONS"
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 PAUSE=0
+SERIAL=stdio
+NOGRAPHIC=0
+DRIVE=temp_drive
 
 while true; do
   case "$1" in
     -h|--help)
       echo "usage: $0 [opts] programs..."
       echo ""
-      echo "  -h --help       Print this help message"
-      echo "  -S --wait       Pause CPU on startup"
+      echo "  -h, --help          Print this help message"
+      echo "  -d, --drive=FILE    Drive to use (default=temp_drive)"
+      echo "  -S, --wait          Pause CPU on startup"
+      echo "  -s, --serial=FILE   Output serial to FILE instead of stdout"
+      echo "  -c, --coverage=FILE Output unique hit IP values to FILE"
+      echo "      --nographic     Don't display the graphics (default is SDL or curses)"
       exit 1
       shift;;
     -S|--wait)
       PAUSE=1
       shift;;
-    --)
-      shift
-      break;;
+    -s|--serial)
+      SERIAL="file:$2"
+      shift; shift;;
+    --nographic)
+      NOGRAPHIC=1
+      shift;;
+    -d|--drive)
+      DRIVE="$2"
+      shift; shift;;
+    -c|--coverage)
+      COVERAGE="$2"
+      shift; shift;;
+    --) shift; break;;
+    *) break ;;
   esac
 done
 
 MODULES="$(join_by , george.multiboot "$@")"
 
 QEMU_ARGS=" -kernel bootloader.multiboot -initrd $MODULES"
-QEMU_ARGS+=" -drive file=temp_drive,index=0,media=disk,format=raw"
-QEMU_ARGS+=" -serial stdio"
+QEMU_ARGS+=" -m 128"
+QEMU_ARGS+=" -drive file=$DRIVE,index=0,media=disk,format=raw"
+QEMU_ARGS+=" -serial $SERIAL"
 QEMU_ARGS+=" -device isa-debug-exit,iobase=0xf4,iosize=0x04"
 # Expose standard gdb on :1234
 QEMU_ARGS+=" -s"
@@ -44,9 +63,19 @@ if [ "$PAUSE" -gt 0 ]; then
   QEMU_ARGS+=" -S"
 fi
 
+if [ "$NOGRAPHIC" -gt 0 ]; then
+  QEMU_ARGS+=" -nographic -display none"
 # If DISPLAY isn't set, use -curses
-if [ -z "$DISPLAY" ]; then
+elif [ -z "$DISPLAY" ]; then
   QEMU_ARGS+=" -curses"
 fi
 
-exec qemu-system-x86_64 $QEMU_ARGS
+#echo "$QEMU_ARGS" >&1
+
+if [ -z "$COVERAGE" ]; then
+  qemu-system-x86_64 $QEMU_ARGS
+  exit $?
+else
+  qemu-system-x86_64 $QEMU_ARGS -d in_asm -D >( grep -E "^(Trace.*\[ffffffff|IN:|0xffffff)" | uniq > "$COVERAGE")
+  exit $?
+fi
