df642861f098bf391a9115be0258bf1f6222d688
Colin Stanfill <colin.stanfill@gmail.com>
2017-08-02 21:00:24 -0400

    Switch all 1G pages to 2M pages
    This is important because kvm doesn't seem to work well with 1GB pages
    (to say the least).
    Also remove SMEP support because it breaks under kvm and doesn't seem
    to be supported under QEMU emulation either right now.

--- bootloader/start32.c
+++ bootloader/start32.c
@@ -14,7 +14,9 @@ uint8_t gdt[][8] = {
 typedef uint64_t pagetable[PAGE_NUM_ENTRIES] __attribute__((aligned(0x1000)));
 
 pagetable bad_pml4;
-pagetable bad_pdpt;
+pagetable low_pdpt;
+pagetable high_pdpt;
+pagetable bad_pds[2];
 
 void check_magic (uint32_t magic) {
   if (magic != 0x2BADB002) {
@@ -33,12 +35,20 @@ void setup_gdt (void) {
   __asm__("lgdt %0" : : "m"(GDT_addr) : );
 }
 
+/* We map the bottom 2GB of memory to three locations: zero, -512GB, and -2GB. */
 void init_page_table (void) {
-  bad_pml4[0] = (uint32_t)(&bad_pdpt[0]) | PAGE_MASK_KERNEL;
-  bad_pml4[511] = (uint32_t)(&bad_pdpt[0]) | PAGE_MASK_KERNEL;
-  for (uint64_t i = 0; i < PAGE_NUM_ENTRIES - 2; i++) {
-    bad_pdpt[i] = (i << 30) | PAGE_MASK_KERNEL | PAGE_MASK_SIZE;
+  bad_pml4[0] = (uint32_t)(&low_pdpt[0]) | PAGE_MASK_KERNEL;
+  bad_pml4[511] = (uint32_t)(&high_pdpt[0]) | PAGE_MASK_KERNEL;
+
+  low_pdpt[0] = (uint32_t)(&bad_pds[0][0]) | PAGE_MASK_KERNEL;
+  low_pdpt[1] = (uint32_t)(&bad_pds[1][0]) | PAGE_MASK_KERNEL;
+  high_pdpt[0] = (uint32_t)(&bad_pds[0][0]) | PAGE_MASK_KERNEL;
+  high_pdpt[1] = (uint32_t)(&bad_pds[1][0]) | PAGE_MASK_KERNEL;
+  high_pdpt[510] = (uint32_t)(&bad_pds[0][0]) | PAGE_MASK_KERNEL;
+  high_pdpt[511] = (uint32_t)(&bad_pds[1][0]) | PAGE_MASK_KERNEL;
+
+  for (uint64_t i = 0; i < PAGE_NUM_ENTRIES; i++) {
+    bad_pds[0][i] = (i << 21) | PAGE_MASK_KERNEL | PAGE_MASK_SIZE;
+    bad_pds[1][i] = ((i + PAGE_NUM_ENTRIES) << 21) | PAGE_MASK_KERNEL | PAGE_MASK_SIZE;
   }
-  bad_pdpt[510] = (0 << 30) | PAGE_MASK_KERNEL | PAGE_MASK_SIZE;
-  bad_pdpt[511] = (1 << 30) | PAGE_MASK_KERNEL | PAGE_MASK_SIZE;
 }
